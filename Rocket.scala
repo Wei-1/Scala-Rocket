23f27532ceb211a76fb5f749549988b9891a611c
miuser <miuser@MI-Secret.ghtinc.com>
2017-10-20 17:18:21 +0800

    smaller-rocket-with-states

--- Rocket.scala
+++ Rocket.scala
@@ -1,27 +1,52 @@
-// Wei Chen 2017-9-08
 import scala.sys.process._
 
 object Rocket extends App {
 
     def clear = "clear" !
 
-    val rocket = """
-    /^A^\    
-     ]=[     
-     ]=[     
-    /{O}\    
-A=^[|=H=|]^=A
-"""
+    def rand: Double = scala.util.Random.nextDouble
 
     def fire = {
-        val tmp = if(scala.util.Random.nextDouble > 0.6) "W" else "V"
-        if (scala.util.Random.nextDouble > 0.8) tmp.toLowerCase else tmp
+        val tmp = if(rand > 0.6) "W" else "V"
+        if (rand > 0.8) tmp.toLowerCase else tmp
     }
 
+    val rocket_m = """
+  ╒▲╕  
+  ├☼┤  
+A=[╦]=A
+"""
+   val rocket_l = """
+ ╒♦    
+ ☼┤    
+A[=A   
+"""
+   val rocket_r = """
+    ♦╕ 
+    ├☼ 
+   A=]A
+"""
+
+    var state = "m"
     while(true) {
+        if(rand > 0.95) {
+            if(state == "m") {
+                state = if(rand >= 0.5) "l" else "r"
+            } else if(state == "l") {
+                state = if(rand >= 0.5) "m" else "r"
+            } else if(state == "r") {
+                state = if(rand >= 0.5) "m" else "l"
+            }
+        }
         clear
-        println(rocket + " " * 5 + fire + fire + fire + " " * 5)
+        
+        if(state == "m") {
+            println(rocket_m + "   " + fire + "   ")
+        } else if(state == "l") {
+            println(rocket_l + " " + fire + fire + "    ")
+        } else if(state == "r") {
+            println(rocket_r + "    " + fire + fire + " ")
+        }
         Thread.sleep(100)
     }
-
 }
